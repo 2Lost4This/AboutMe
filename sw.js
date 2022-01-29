4d38b5b3e8e2b724cff81559fab4e3d6788d8adf
InvalidLenni <68335355+InvalidLenni@users.noreply.github.com>
2022-01-29 01:29:21 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+importScripts('https://arc.io/arc-sw-core.js') importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'); workbox.routing.registerRoute( /.*/, new workbox.strategies.NetworkFirst() );
